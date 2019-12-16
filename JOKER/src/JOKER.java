3d1e13599e2a66e528d7aeb2f5a3a8a832c4e114
margaridarp <r20191195@novaims.unl.pt>
2019-12-16 11:04:44 +0000

    texto 1

---
+++ JOKER/src/JOKER.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+public class JOKER {
+    public static void main(String[] args) throws InterruptedException {
+    	
+    	Scanner sc = new Scanner (System.in);
+
+        // QUESTION'S RELATED VARIABLES
+        String[] questions = Questions.questions.clone();
+        String[][] allOptions = Questions.options.clone();
+        int[] correctAnswers = Questions.correctAnswers.clone();
+
+        // VARIABLES -----------------------//
+        int[] moneyTree = {0, 200, 500, 1000, 3000, 10000, 50000};
+        int moneyTreeIndex = 0;
+        int jokers = 7;
+        Scanner scanner = new Scanner(System.in);
+
+        // GAME OPENING
+        Utils.printGreeting("JOKER", "$", false);
+
+        // GAME LOOP
+        // For loop
+            // Imprimir Status
+            // Imprimir Questões
+
+            // Definir array de booleans para indicar quais opções de respostas serão apresentadas.
+            // Definir variável de controle do Do-While loop abaixo
+
+            // Do-While loop
+                // Imprimir Opções de Respostas
+                // Se o Concorrente escolher uma das opções
+                    // Atualizar a variável de controle do Do-While loop para interrompê-lo
+                    // Se a resposta do Concorrente estiver correta
+                        // Atualizar os valores adequados
+                        // Prover feedback ao Concorrente
+                    // Se a resposta do Concorrente estiver errada
+                        // Penalizar os valores adequados (dica: são dois valores; considerar um array – veja o método `penalizePlayer()`)
+                        // Prover feedback ao Concorrente
+                // Senão Se o Concorrente escolher um Joker
+                    // Se houver Joker disponível
+                        // Alterar o array de booleans que indica as opções a serem apresentadas (omitir uma incorreta)
+                        // Atualizar o número de Jokers (atenção para o valor não ser negativo)
+                    // Senão
+                        // Prover feedback ao Concorrente de que não há Jokers
+                // Senão
+                    // Prover feedback de que o input não é válido.
+
+        // Imprimir conclusão do Jogo
+    }
+    
+//MÉTODOS
+    
+    static void printStatus(int round, int moneyEarned, int jokers) {
+    	
+    	// DIZER AO UTILIZADOR EM QUE RONDA ESTÁ, QUANDO DINHEIRO TEM E/OU GANHOU E QUANTOS JOKERS TEM
+    }
+
+    static void printQuestion(int round, String question) {
+    	// METER AS PERGUNTAS
+    	
+    	for(round=0; round<12; round++) {
+    		System.out.println(question);}
+    	
+    }
+
+    static void printOptions(String[] options, boolean[] includedOptions) {
+    	// METER AS OPÇÕES E INDICAR QUAL A CERTA E QUAIS AS ERRADAS
+    	
+    
+    }
+
+    static int readAnswer(Scanner sc) {
+    	// RECEBER A RESPOSTA DO UTILIZADOR
+    	
+    	System.out.println("Qual a resposta que gostaria de bloquear?");
+    	char answer = sc.next().charAt(0);
+    	
+    	return answer;
+    }
+
+    static void printOutcomeMessage(boolean answerIsCorrect) {
+    
+    	System.out.println("A resposta correta é: ");
+    	
+    }
+
+    static boolean[] applyJoker(boolean[] includedOptions, int correctAnswer) {
+    	// DIZER AO UTILIZADOR SE A RESPOSTA ESTÁ CERTA OU NÃO
+    	
+    	if() {
+    		System.out.println("A resposta está correta!");
+    	} else {
+    		System.out.println("A resposta está errada!");
+    	}
+    }
+   
+    static int safelyDecreaseJoker(int jokers) {
+    	// O UTILIZADOR UTILIZOU UM JOKER
+    	
+    	System.out.println("O utilizador tem "+jokers+".");
+    	return jokers;
+    }
+
+    static int[] penalizePlayer(int jokers, int moneyTreeIndex) {
+    	// A RESPOSTA ESTÁ ERRADO LOGO APLICAR AS CONSEQUÊNCIAS
+    	
+    }
+
+    static void printConclusion(int moneyEarned, int jokers) {
+    	// SITUAÇÃO CORRENTE: SE GANHOU DINHEIRO OU PERDEU, QUANTOS JOKERS TEM
+    
+    	System.out.println("Ganhou " +moneyEarned+ ("euros. Tem, neste momento, "+jokers+" jokers."));
+    }
+}
